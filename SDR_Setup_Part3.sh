ea983245bdda76434ba6fb52486a929433b36173
Colby Gilbert <92291342+cgilbert11@users.noreply.github.com>
2022-03-29 00:01:20 -0400

    Create SDR_Setup_Part3.sh

---
+++ SDR_Setup_Part3.sh
@@ -0,0 +1,6 @@
+echo "Completing Audio Mapping"
+sudo apt-get install pulseaudio pavucontrol mplayer -y
+pulseaudio load-module module-null-sink sink_name=Virtual_Sink sink_properties=device.description=Virtual_Sink
+pulseaudio -k
+
+echo "Done with SDR Setup!"
